6b8234e2d827b0c4a42834948af42d71ab3db464
Krotov Pavel <krotovpv@gmail.com>
2023-04-13 13:50:35 +0300

    Первая запись

--- Viscometer/Form1.cs
+++ Viscometer/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 
 namespace Viscometer
 {
+    //для гита
     public partial class Form1 : Form
     {
         public Form1()
@@ -22,7 +23,7 @@ namespace Viscometer
         {
             try
             {
-                SerialPort serialPort = new SerialPort("COM3");
+                SerialPort serialPort = new SerialPort("COM6");
                 serialPort.DataReceived += SerialPort_DataReceived;
                 serialPort.Open();
             }
