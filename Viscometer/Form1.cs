87caf4071305d6b0fabf454071e96749138f7af1
Krotov Pavel <krotovpv@gmail.com>
2023-04-18 14:47:07 +0300

    Добавлен helper в который добавлен вспомогательный метод инвока. Переписаны обращения к контролам через вспомогательный метод.

--- Viscometer/Form1.cs
+++ Viscometer/Form1.cs
@@ -66,75 +66,73 @@ namespace Viscometer
         {
             if (line == "") return;
             //записываем строку в базу
-            Action act = () =>
+
+            txtData.InvokeEx(() => 
             {
                 txtData.AppendText(line + Environment.NewLine);
                 txtData.SelectionStart = txtData.Text.Length;
                 txtData.ScrollToCaret();
-            };
-            if (txtData.InvokeRequired)
-                txtData.Invoke(act);
-            else
-                act();
+            });
 
             string[] arr = line.Split(',');
-            if (arr.Length > 0)
+            if (arr[0] == "L" && arr[1][0] == 'a')
             {
-                if (arr[0] == "L" && arr[1][0] == 'a')
+                string[] timeArr = arr[1].Trim(' ', 'a').Split(':', '.');
+                TimeSpan time = new TimeSpan(0, Convert.ToInt16(timeArr[0]), Convert.ToInt16(timeArr[1]));
+                if (arr[2][0] == 'b')
                 {
-                    string[] timeArr = arr[1].Trim(' ', 'a').Split(':', '.');
-                    TimeSpan time = new TimeSpan(0, Convert.ToInt16(timeArr[0]), Convert.ToInt16(timeArr[1]));
-                    if (arr[2][0] == 'b')
-                    {
-                        double value = Convert.ToDouble(arr[2].Replace('.', ',').Replace(" ", "").Replace("b", ""));
+                    double value = Convert.ToDouble(arr[2].Replace('.', ',').Replace(" ", "").Replace("b", ""));
 
-                        Action actChart = () => chartValue.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(time.TotalSeconds, value));
-                        if (chartValue.InvokeRequired)
-                            chartValue.Invoke(actChart);
-                        else
-                            actChart();
-                    }
-                    if (arr[3][0] == 'd' && arr[4][0] == 'e')
-                    {
-                        double valueD = Convert.ToDouble(arr[3].Replace('.', ',').Replace(" ", "").Replace("d", ""));
-                        double valueE = Convert.ToDouble(arr[4].Replace('.', ',').Replace(" ", "").Replace("e", ""));
+                    chartValue.InvokeEx(() =>
+                        chartValue.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(time.TotalSeconds, value)));
+                }
+                if (arr[3][0] == 'd' && arr[4][0] == 'e')
+                {
+                    double valueD = Convert.ToDouble(arr[3].Replace('.', ',').Replace(" ", "").Replace("d", ""));
+                    double valueE = Convert.ToDouble(arr[4].Replace('.', ',').Replace(" ", "").Replace("e", ""));
 
-                        Action actChart = () =>
-                        {
-                            chartTemperature.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(time.TotalSeconds, valueD));
-                            chartTemperature.Series[1].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(time.TotalSeconds, valueE));
-                        };
-                        if (chartTemperature.InvokeRequired)
-                            chartTemperature.Invoke(actChart);
-                        else
-                            actChart();
-                    }
-                }
-                else if (arr[0] == "SV1L")
-                {
-                    foreach (var item in arr)
+                    chartTemperature.InvokeEx(() => 
                     {
-                        switch (item[0])
-                        {
-                            case 'A':
-                                lblTemperature.Text = "Температура: " + item.Remove(0, 1); break;
-                            case 'B':
-                                lblTestTime.Text = "Время испытания: " + item.Remove(0, 1); break;
-                            case 'C':
-                                lblPreheat.Text = "Время прогрева: " + item.Remove(0, 1); break;
-                            case 'D':
-                                lblRelax.Text = "Время релаксации: " + item.Remove(0, 1); break;
-                            case 'F':
-                                lblNum.Text = "Номер: " + item.Remove(0, 1); break;
-                            default:
-                                break;
-                        }
+                        chartTemperature.Series[0].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(time.TotalSeconds, valueD));
+                        chartTemperature.Series[1].Points.Add(new System.Windows.Forms.DataVisualization.Charting.DataPoint(time.TotalSeconds, valueE));
+                    });
+                }
+            }
+            else if (arr[0] == "SV1L")
+            {
+                foreach (var item in arr)
+                {
+                    switch (item[0])
+                    {
+                        case 'A':
+                            lblTemperature.InvokeEx(() =>
+                                lblTemperature.Text = "Температура: " + item.Remove(0, 1));
+                            break;
+                        case 'B':
+                            lblTestTime.InvokeEx(() => 
+                                lblTestTime.Text = "Время испытания: " + item.Remove(0, 1)); 
+                            break;
+                        case 'C':
+                            lblPreheat.InvokeEx(() =>
+                                lblPreheat.Text = "Время прогрева: " + item.Remove(0, 1));
+                            break;
+                        case 'D':
+                            lblRelax.InvokeEx(() =>
+                                lblRelax.Text = "Время релаксации: " + item.Remove(0, 1));
+                            break;
+                        case 'F':
+                            lblNum.InvokeEx(() =>
+                                lblNum.Text = "Номер: " + item.Remove(0, 1));
+                            break;
+                        default:
+                            break;
                     }
                 }
-                else if (arr[0][0] == 'E')
-                {
-                    lblResult.Text = $"{arr[1]}, {arr[2]}, {arr[3]}, {arr[4]}, {arr[5]}";
-                }
+            }
+            else if (arr[0][0] == 'E')
+            {
+                lblResult.InvokeEx(() =>
+                    lblResult.Text = $"{arr[1]}, {arr[2]}, {arr[3]}, {arr[4]}, {arr[5]}");
             }
         }
     }
---
+++ Viscometer/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Viscometer
+{
+    public static class Helper
+    {
+        public static void InvokeEx(this Control ctl, Action act)
+        {
+            if (ctl == null) return;
+            if (ctl.InvokeRequired == true)
+                ctl.Invoke(act);
+            else
+                act();
+        }
+    }
+}
