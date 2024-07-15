2915a4510cc15e53f9ef06ae31c8c136f031044f
Krotov Pavel <krotovpv@gmail.com>
2024-07-15 14:36:31 +0300

    Изменен класс Testers

--- Viscometer/Tester.cs
+++ Viscometer/Tester.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using static Viscometer.Tester;
 
 namespace Viscometer
 {
     public static class Tester
     {
         public static bool IsAuthorization { get; private set; } = false;
-        public static short Id { get; private set; } = 0;
+        public static short Id { get; private set; }
         public static string Name { get; private set; } = string.Empty;
-        public static short? IdSub { get; private set; } = null;
+        public static short IdSub { get; private set; }
         public static string NameSub { get; private set; } = string.Empty;
-        public static string Rights { get; private set; } = "MyOrders";
+        public static Rights Right { get; private set; } = Rights.MyOrders;
 
         public static bool Authorization(string NameUser, string Password)
         {
@@ -24,20 +25,21 @@ namespace Viscometer
             {
                 IsAuthorization = true;
                 Id = dt.Rows[0].Field<short>("idTester");
-                Name = dt.Rows[0].Field<string>("nameTester");
-                IdSub = dt.Rows[0].Field<short?>("idSubdiv");
-
-                if (IdSub != null)
-                {
-                    NameSub = DataBase.GetData("SELECT [nameSubdiv] FROM [Subdivisions] WHERE [idSubdiv]='" + IdSub + "'").Rows[0].Field<string>("nameSubdiv");
-                }
-
-                short idRights = dt.Rows[0].Field<short>("idRights");
-                Rights = DataBase.GetData("SELECT [nameRights] FROM [Rights] WHERE [idRights]='" + idRights.ToString() + "'").Rows[0].Field<string>("nameRights");
+                Name = dt.Rows[0].Field<string>("nameTester") ?? string.Empty;
+                IdSub = dt.Rows[0].Field<short>("idSubdiv");
+                NameSub = DataBase.GetData("SELECT [nameSubdiv] FROM [Subdivisions] WHERE [idSubdiv]='" + IdSub + "'").Rows[0].Field<string>("nameSubdiv") ?? string.Empty;
+                Right = (Rights)dt.Rows[0].Field<short>("idRights");
                 return true;
             }
 
             return false;
+        }
+
+        public enum Rights
+        {
+            AllSub = 1,
+            MySub = 2,
+            MyOrders = 3
         }
     }
 }
